4f988cc1a8ffee29775ba5908c9899d842555f3f
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-04-11 10:49:44 +0300

    updated the 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
-const STORAGE_KEY = 'feedback-form-state';
 
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
@@ -27,8 +27,10 @@ function fillFormFields() {
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(formData);
-
+  if (email.value === '' || message.value === '') {
+    return alert('Будь ласка, заповніть всі поля');
+  }
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
